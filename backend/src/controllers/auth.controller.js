ccbc2822970514ad9335516cffedacd9a48d8043
vnhan28 <vnhan2808@gmail.com>
2025-10-05 19:00:41 +0700

    fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,8 +30,8 @@ export const signUp = async (req, res) =>{
         })
 
         if(newUser) {
-            generateToken(newUser._id, res)
-            await newUser.save()
+            const saveUser = await newUser.save()
+            generateToken(saveUser._id, res)
             res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
