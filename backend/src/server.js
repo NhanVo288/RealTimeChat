beec623775f4abfd8e8eef43ad180d7ce6950fa5
vnhan28 <vnhan2808@gmail.com>
2025-10-05 12:17:54 +0700

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,20 @@ import authRoute from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 dotenv.config()
 
 const app = express()
-
+const __dirname = path.resolve()
 const PORT = process.env.PORT
 
 app.use("/api/auth",authRoute)
 app.use("/api/messages",messageRoute)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
 app.listen(3000, () => console.log("server running on port:", PORT))
