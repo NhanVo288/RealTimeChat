49729f72e683b108d639dcac0869750a0f64fb54
vnhan28 <vnhan2808@gmail.com>
2025-10-06 19:40:22 +0700

    set up frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 
 export const useAuthStore = create((set) => ({
-    authUser: {name,id,age},
+    authUser: {name: "hi",id: "2",age: "12"},
     isLoggedin: false,
     isLoading:false
 }))
