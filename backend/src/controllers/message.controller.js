47196cdd382b4c83b1dda31b26946f84682fccfb
vnhan28 <vnhan2808@gmail.com>
2025-10-06 17:26:01 +0700

    add message enpoint

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,77 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../model/Message.js";
+import User from "../model/User.js";
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedUserId = req.user._id;
+    const filterdUser = await User.find({ _id: { $ne: loggedUserId } }).select(
+      "-password"
+    );
+    res.status(200).json(filterdUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "SERVER ERROR" });
+  }
+};
+
+export const getChatByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const { id: userChatWith } = req.params;
+    //find:  i send orthers message or others send me message
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userChatWith },
+        { senderId: userChatWith, receiverId: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageURL;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageURL = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageURL,
+    });
+    await newMessage.save();
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getChats = async (req, res) => {
+  try {
+    const loggedUserId = req.user._id;
+    const messsages = await Message.find({
+      $or: [{ senderId: loggedUserId }, { receiverId: loggedUserId }],
+    });
+
+    const chatPartnerId = [...new Set(messsages.map((msg) =>
+      msg.senderId.toString() === loggedUserId.toString()
+        ? msg.receiverId.toString()
+        : msg.senderId.toString()
+    ))];
+
+    const chatPartners = await User.find({_id:{$in: chatPartnerId}}).select("-password")
+    res.status(200).json(chatPartners)
+  } catch (error) {
+    console.log(error)
+  }
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,12 @@
 import express from 'express'
-
+import { getAllContacts, getChatByUserId, sendMessage, getChats } from '../controllers/message.controller.js'
+import { protectRoute } from '../middleware/auth.middleware.js'
+import { arcjetProtect } from '../middleware/arcjet.middleware.js'
 const router = express.Router()
-
-router.get("/send", (req,res) => {
-    res.send('Send Message');
-})
+router.use(arcjetProtect,protectRoute)
+router.get("/contacts",  getAllContacts)
+router.get("/chats", getChats)
+router.get("/:id", getChatByUserId)
+router.post("/send/:id", sendMessage )
 
 export default router
