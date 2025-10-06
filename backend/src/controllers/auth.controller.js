2fe9498c576ab79cdfc08066368b8a505f962702
vnhan28 <vnhan2808@gmail.com>
2025-10-06 15:13:23 +0700

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -61,6 +61,7 @@ export const signUp = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
+    if(!email || !password ) return res.status(400).json({message: "Email and Password are required"})
     const user = await User.findOne({email});
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
